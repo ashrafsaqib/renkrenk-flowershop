c3fb16319f17eb307291d678e414b35f65522765
Saqib Ashraf <linktosaqib@gmail.com>
2025-11-25 21:27:54 +0500

    slide in cart

--- catalog/controller/common/cart.php
+++ catalog/controller/common/cart.php
@@ -72,7 +72,7 @@ class Cart extends \Opencart\System\Engine\Controller {
 			}
 
 			$data['products'][] = [
-				'thumb'        => $this->model_tool_image->resize($product['image'], $this->config->get('config_image_cart_width'), $this->config->get('config_image_cart_height')),
+				'thumb'        => $this->model_tool_image->resize($product['image'], 80,80),
 				'subscription' => $subscription,
 				'price'        => $price_status ? $product['price_text'] : '',
 				'total'        => $price_status ? $product['total_text'] : '',
@@ -102,7 +102,92 @@ class Cart extends \Opencart\System\Engine\Controller {
 	 * @return void
 	 */
 	public function info(): void {
-		$this->response->setOutput($this->index());
+		$this->load->language('common/cart');
+
+		$totals = [];
+		$taxes = $this->cart->getTaxes();
+		$total = 0;
+
+		// Cart
+		$this->load->model('checkout/cart');
+
+		// Image
+		$this->load->model('tool/image');
+
+		// Display prices
+		if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+			($this->model_checkout_cart->getTotals)($totals, $taxes, $total);
+
+			$price_status = true;
+		} else {
+			$price_status = false;
+		}
+
+		// Products
+		$data['products'] = [];
+
+		$products = $this->model_checkout_cart->getProducts();
+
+		foreach ($products as $product) {
+			if ($product['option']) {
+				foreach ($product['option'] as $key => $option) {
+					if ($option['type'] != 'file') {
+						$value = $option['value'];
+					} else {
+						$upload_info = $this->model_tool_upload->getUploadByCode($option['value']);
+
+						if ($upload_info) {
+							$value = $upload_info['name'];
+						} else {
+							$value = '';
+						}
+					}
+
+					$product['option'][$key]['value'] = (oc_strlen($value) > 20 ? oc_substr($value, 0, 20) . '..' : $value);
+				}
+			}
+
+			$subscription = '';
+
+			if ($product['subscription']) {
+				if ($product['subscription']['duration']) {
+					$subscription .= sprintf($this->language->get('text_subscription_duration'), $price_status ?? $product['subscription']['price_text'], $product['subscription']['cycle'], $product['subscription']['frequency'], $product['subscription']['duration']);
+				} else {
+					$subscription .= sprintf($this->language->get('text_subscription_cancel'), $price_status ?? $product['subscription']['price_text'], $product['subscription']['cycle'], $product['subscription']['frequency']);
+				}
+			}
+
+			$data['products'][] = [
+				'thumb'        => $this->model_tool_image->resize($product['image'], 120, 120),
+				'subscription' => $subscription,
+				'price'        => $price_status ? $product['price_text'] : '',
+				'total'        => $price_status ? $product['total_text'] : '',
+				'href'         => $this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&product_id=' . $product['product_id'])
+			] + $product;
+		}
+
+		// Totals
+		$data['totals'] = [];
+
+		foreach ($totals as $total) {
+			$data['totals'][] = ['text' => $this->currency->format($total['value'], $this->session->data['currency'])] + $total;
+		}
+
+		$data['list'] = $this->url->link('common/cart.info', 'language=' . $this->config->get('config_language'));
+		$data['remove'] = $this->url->link('common/cart.remove', 'language=' . $this->config->get('config_language'));
+
+		$data['cart'] = $this->url->link('checkout/cart', 'language=' . $this->config->get('config_language'));
+		$data['checkout'] = $this->url->link('checkout/checkout', 'language=' . $this->config->get('config_language'));
+
+		$data['text_cart'] = $this->language->get('text_cart');
+		$data['text_checkout'] = $this->language->get('text_checkout');
+		$data['text_no_results'] = $this->language->get('text_no_results');
+		$data['text_model'] = $this->language->get('text_model');
+		$data['text_subscription'] = $this->language->get('text_subscription');
+		$data['text_points'] = $this->language->get('text_points');
+		$data['button_remove'] = $this->language->get('button_remove');
+
+		$this->response->setOutput($this->load->view('common/cart', $data));
 	}
 
 	/**
