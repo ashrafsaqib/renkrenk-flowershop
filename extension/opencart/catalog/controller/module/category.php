8d1063bd98055efe7e624217b34a2a236b6e03c9
Saqib Ashraf <linktosaqib@gmail.com>
2026-01-20 16:20:55 +0500

    Update category.php

--- extension/opencart/catalog/controller/module/category.php
+++ extension/opencart/catalog/controller/module/category.php
@@ -40,39 +40,65 @@ class Category extends \Opencart\System\Engine\Controller {
 
 		$data['categories'] = [];
 
-		$categories = $this->model_catalog_category->getCategories(0);
-
-		foreach ($categories as $category) {
-			$children_data = [];
-
-			if ($category['category_id'] == $data['category_id']) {
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
-
-				foreach ($children as $child) {
+		// Check if we're on a category page and if it has subcategories
+		$show_only_subcategories = false;
+		if ($data['category_id']) {
+			$subcategories = $this->model_catalog_category->getCategories($data['category_id']);
+			if (!empty($subcategories)) {
+				$show_only_subcategories = true;
+				// Show only the subcategories of the current category
+				foreach ($subcategories as $child) {
 					$filter_data = [
 						'filter_category_id'  => $child['category_id'],
 						'filter_sub_category' => true
 					];
 
-					$children_data[] = [
+					$data['categories'][] = [
 						'category_id' => $child['category_id'],
 						'name'        => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-						'href'        => $this->url->link('product/category', 'language=' . $this->config->get('config_language') . '&path=' . $category['category_id'] . '_' . $child['category_id'])
+						'children'    => [],
+						'href'        => $this->url->link('product/category', 'language=' . $this->config->get('config_language') . '&path=' . $data['category_id'] . '_' . $child['category_id'])
 					];
 				}
 			}
+		}
 
-			$filter_data = [
-				'filter_category_id'  => $category['category_id'],
-				'filter_sub_category' => true
-			];
+		// If not showing only subcategories, show all top-level categories
+		if (!$show_only_subcategories) {
+			$categories = $this->model_catalog_category->getCategories(0);
 
-			$data['categories'][] = [
-				'category_id' => $category['category_id'],
-				'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-				'children'    => $children_data,
-				'href'        => $this->url->link('product/category', 'language=' . $this->config->get('config_language') . '&path=' . $category['category_id'])
-			];
+			foreach ($categories as $category) {
+				$children_data = [];
+
+				if ($category['category_id'] == $data['category_id']) {
+					$children = $this->model_catalog_category->getCategories($category['category_id']);
+
+					foreach ($children as $child) {
+						$filter_data = [
+							'filter_category_id'  => $child['category_id'],
+							'filter_sub_category' => true
+						];
+
+						$children_data[] = [
+							'category_id' => $child['category_id'],
+							'name'        => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+							'href'        => $this->url->link('product/category', 'language=' . $this->config->get('config_language') . '&path=' . $category['category_id'] . '_' . $child['category_id'])
+						];
+					}
+				}
+
+				$filter_data = [
+					'filter_category_id'  => $category['category_id'],
+					'filter_sub_category' => true
+				];
+
+				$data['categories'][] = [
+					'category_id' => $category['category_id'],
+					'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+					'children'    => $children_data,
+					'href'        => $this->url->link('product/category', 'language=' . $this->config->get('config_language') . '&path=' . $category['category_id'])
+				];
+			}
 		}
 
 		return $this->load->view('extension/opencart/module/category', $data);
